2dc7d87df8215dc56d200c792587013be46dca75
BugAgain <bug.errorcode5@gmail.com>
2017-10-29 20:05:02 -0200

    Some xml docs for the StreamExtensions as part of the issue #44

--- src/Request/StreamExtensions.cs
+++ src/Request/StreamExtensions.cs
@@ -7,7 +7,12 @@
 
     public static class StreamExtensions
     {
-
+        /// <summary>
+        /// Gets the RequestBody Stream as string in the specified <see cref="Encoding"/> (optional)
+        /// </summary>
+        /// <param name="stream">Current <see cref="Stream"/></param>
+        /// <param name="encoding">The character encoding to use or <see cref="Encoding.UTF8"/> by default</param>
+        /// <returns>Current string content of the RequestBody Stream</returns>
         public static string AsString(this Stream stream, Encoding encoding = null)
         {
             using (var reader = new StreamReader(stream, encoding ?? Encoding.UTF8))
@@ -23,6 +28,13 @@
             }
         }
 
+        /// <summary>
+        /// Gets the RequestBody Stream as string asynchronously in the specified <see cref="Encoding"/> (optional)
+        /// </summary>
+        /// <param name="stream">Current <see cref="Stream"/></param>
+        /// <param name="encoding">The character encoding to use or <see cref="Encoding.UTF8"/> by default</param>
+        /// <param name="cancellationToken">The cancellation instruction if required</param>
+        /// <returns>Awaited <see cref="Task"/></returns>
         public static async Task<string> AsStringAsync(this Stream stream, Encoding encoding = null, CancellationToken cancellationToken = default(CancellationToken))
         {
             using (var reader = new StreamReader(stream, encoding ?? Encoding.UTF8))
