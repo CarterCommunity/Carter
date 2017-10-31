62b5a61eea1baf434981caaf34ebd66363cb1b4f
Peter Wetzel <pdwetz@gmail.com>
2017-10-30 20:06:21 -0400

    XML docs added for response and route data methods
    Part of #44 efforts

--- src/Request/RouteDataExtensions.cs
+++ src/Request/RouteDataExtensions.cs
@@ -1,10 +1,16 @@
 namespace Botwin.Request
 {
-    using System;
     using Microsoft.AspNetCore.Routing;
 
     public static class RouteDataExtensions
     {
+        /// <summary>
+        /// Retrieve strongly typed route value for given key
+        /// </summary>
+        /// <typeparam name="T">Route value data type</typeparam>
+        /// <param name="routeData"><see cref="RouteData"/></param>
+        /// <param name="key">Route key</param>
+        /// <returns>Route value for given key</returns>
         public static T As<T>(this RouteData routeData, string key)
         {
             var value = routeData.Values[key];
