125a4fcd676a42adbacabfb13035f543bf04d323
Jonathan Channon <jonathan.channon@gmail.com>
2017-11-08 21:29:24 +0000

    Add xml docs for Botwin module

--- src/BotwinModule.cs
+++ src/BotwinModule.cs
@@ -6,20 +6,36 @@ namespace Botwin
     using Microsoft.AspNetCore.Http;
     using Microsoft.AspNetCore.Routing;
 
+    /// <summary>
+    /// A class for defining routes in your Botwin application
+    /// </summary>
     public class BotwinModule
     {
         public readonly List<(string verb, string path, RequestDelegate handler)> Routes;
 
         private readonly string basePath;
 
+        /// <summary>
+        /// A handler that can be invoked before the defined route
+        /// </summary>
         public Func<HttpContext, Task<bool>> Before { get; set; } 
 
+        /// <summary>
+        /// A handler that can be invoked after the defined route
+        /// </summary>
         public RequestDelegate After { get; protected set; }
 
+        /// <summary>
+        /// Initializes a new instance of <see cref="BotwinModule"/>
+        /// </summary>
         protected BotwinModule() : this(string.Empty)
         {
         }
 
+        /// <summary>
+        /// Initializes a new instance of <see cref="BotwinModule"/>
+        /// </summary>
+        /// <param name="basePath">A base path to group routes in your <see cref="BotwinModule"/></param>
         protected BotwinModule(string basePath)
         {
             this.Routes = new List<(string verb, string path, RequestDelegate handler)>();
@@ -27,12 +43,22 @@ namespace Botwin
             this.basePath = this.RemoveEndingSlash(cleanPath);
         }
 
+        /// <summary>
+        /// Declares a route for GET requests
+        /// </summary>
+        /// <param name="path">The path for your route</param>
+        /// <param name="handler">The handler that is invoked when the route is hit</param>
         protected void Get(string path, Func<HttpRequest, HttpResponse, RouteData, Task> handler)
         {
             Task RequestDelegate(HttpContext httpContext) => handler(httpContext.Request, httpContext.Response, httpContext.GetRouteData());
             this.Get(path, RequestDelegate);
         }
 
+        /// <summary>
+        /// Declares a route for GET requests
+        /// </summary>
+        /// <param name="path">The path for your route</param>
+        /// <param name="handler">The handler that is invoked when the route is hit</param>
         protected void Get(string path, RequestDelegate handler)
         {
             path = this.RemoveStartingSlash(path);
@@ -41,11 +67,22 @@ namespace Botwin
             this.Routes.Add((HttpMethods.Head, path, handler));
         }
 
+        /// <summary>
+        /// Declares a route for POST requests
+        /// </summary>
+        /// <param name="path">The path for your route</param>
+        /// <param name="handler">The handler that is invoked when the route is hit</param>
         protected void Post(string path, Func<HttpRequest, HttpResponse, RouteData, Task> handler)
         {
             Task RequestDelegate(HttpContext httpContext) => handler(httpContext.Request, httpContext.Response, httpContext.GetRouteData());
             this.Post(path, RequestDelegate);
         }
+
+        /// <summary>
+        /// Declares a route for POST requests
+        /// </summary>
+        /// <param name="path">The path for your route</param>
+        /// <param name="handler">The handler that is invoked when the route is hit</param>
 
         protected void Post(string path, RequestDelegate handler)
         {
@@ -54,12 +91,22 @@ namespace Botwin
             this.Routes.Add((HttpMethods.Post, path, handler));
         }
 
+        /// <summary>
+        /// Declares a route for DELETE requests
+        /// </summary>
+        /// <param name="path">The path for your route</param>
+        /// <param name="handler">The handler that is invoked when the route is hit</param>
         protected void Delete(string path, Func<HttpRequest, HttpResponse, RouteData, Task> handler)
         {
             Task RequestDelegate(HttpContext httpContext) => handler(httpContext.Request, httpContext.Response, httpContext.GetRouteData());
             this.Delete(path, RequestDelegate);
         }
 
+        /// <summary>
+        /// Declares a route for DELETE requests
+        /// </summary>
+        /// <param name="path">The path for your route</param>
+        /// <param name="handler">The handler that is invoked when the route is hit</param>
         protected void Delete(string path, RequestDelegate handler)
         {
             path = this.RemoveStartingSlash(path);
@@ -67,12 +114,22 @@ namespace Botwin
             this.Routes.Add((HttpMethods.Delete, path, handler));
         }
 
+        /// <summary>
+        /// Declares a route for PUT requests
+        /// </summary>
+        /// <param name="path">The path for your route</param>
+        /// <param name="handler">The handler that is invoked when the route is hit</param>
         protected void Put(string path, Func<HttpRequest, HttpResponse, RouteData, Task> handler)
         {
             Task RequestDelegate(HttpContext httpContext) => handler(httpContext.Request, httpContext.Response, httpContext.GetRouteData());
             this.Put(path, RequestDelegate);
         }
 
+        /// <summary>
+        /// Declares a route for PUT requests
+        /// </summary>
+        /// <param name="path">The path for your route</param>
+        /// <param name="handler">The handler that is invoked when the route is hit</param>
         protected void Put(string path, RequestDelegate handler)
         {
             path = this.RemoveStartingSlash(path);
@@ -80,12 +137,22 @@ namespace Botwin
             this.Routes.Add((HttpMethods.Put, path, handler));
         }
 
+        /// <summary>
+        /// Declares a route for HEAD requests
+        /// </summary>
+        /// <param name="path">The path for your route</param>
+        /// <param name="handler">The handler that is invoked when the route is hit</param>
         protected void Head(string path, Func<HttpRequest, HttpResponse, RouteData, Task> handler)
         {
             Task RequestDelegate(HttpContext httpContext) => handler(httpContext.Request, httpContext.Response, httpContext.GetRouteData());
             this.Head(path, RequestDelegate);
         }
 
+        /// <summary>
+        /// Declares a route for HEAD requests
+        /// </summary>
+        /// <param name="path">The path for your route</param>
+        /// <param name="handler">The handler that is invoked when the route is hit</param>
         protected void Head(string path, RequestDelegate handler)
         {
             path = this.RemoveStartingSlash(path);
@@ -93,6 +160,11 @@ namespace Botwin
             this.Routes.Add((HttpMethods.Head, path, handler));
         }
 
+        /// <summary>
+        /// Declares a route for PATCH requests
+        /// </summary>
+        /// <param name="path">The path for your route</param>
+        /// <param name="handler">The handler that is invoked when the route is hit</param>
         protected void Patch(string path, Func<HttpRequest, HttpResponse, RouteData, Task> handler)
         {
             Task RequestDelegate(HttpContext httpContext) => handler(httpContext.Request, httpContext.Response, httpContext.GetRouteData());
@@ -100,6 +172,11 @@ namespace Botwin
             this.Patch(path, RequestDelegate);
         }
 
+        /// <summary>
+        /// Declares a route for PATCH requests
+        /// </summary>
+        /// <param name="path">The path for your route</param>
+        /// <param name="handler">The handler that is invoked when the route is hit</param>
         protected void Patch(string path, RequestDelegate handler)
         {
             path = this.RemoveStartingSlash(path);
@@ -107,12 +184,22 @@ namespace Botwin
             this.Routes.Add((HttpMethods.Patch, path, handler));
         }
 
+        /// <summary>
+        /// Declares a route for OPTIONS requests
+        /// </summary>
+        /// <param name="path">The path for your route</param>
+        /// <param name="handler">The handler that is invoked when the route is hit</param>
         protected void Options(string path, Func<HttpRequest, HttpResponse, RouteData, Task> handler)
         {
             Task RequestDelegate(HttpContext httpContext) => handler(httpContext.Request, httpContext.Response, httpContext.GetRouteData());
             this.Options(path, RequestDelegate);
         }
 
+        /// <summary>
+        /// Declares a route for OPTIONS requests
+        /// </summary>
+        /// <param name="path">The path for your route</param>
+        /// <param name="handler">The handler that is invoked when the route is hit</param>
         protected void Options(string path, RequestDelegate handler)
         {
             path = this.RemoveStartingSlash(path);
