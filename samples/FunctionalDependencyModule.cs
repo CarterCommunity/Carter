dda51951ed0d03e6e839c121d7bbf164219c1b1b
Jonathan Channon <jonathan.channon@gmail.com>
2017-12-01 19:39:26 +0000

    Fixed #71 and added functional non-ioc sample

---
+++ samples/FunctionalDependencyModule.cs
@@ -0,0 +1,54 @@
+﻿namespace Botwin.Samples
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Botwin.Response;
+
+    public class FunctionalDependencyModule : BotwinModule
+    {
+        public FunctionalDependencyModule()
+        {
+            this.Get("/functional", async (req, res, routeData) =>
+            {
+                var data = Composition.ComposeFunctionalHandler();
+
+                var actor = data();
+
+                await res.Negotiate(actor);
+            });
+        }
+    }
+
+    public class Composition
+    {
+        public static Func<Actor> ComposeFunctionalHandler()
+        {
+            return () => FunctionalRoute.Handle(() =>
+            {
+                Console.WriteLine($"Getting sql connection from settings {AppConfiguration.ConnectionString}");
+                return new[] { new Actor() };
+            }, () => true);
+        }
+    }
+
+    public class AppConfiguration
+    {
+        public static string ConnectionString { get; set; }
+    }
+
+    public static class FunctionalRoute
+    {
+        public static Actor Handle(Func<IEnumerable<Actor>> getActors, Func<bool> userAllowed)
+        {
+            var currentUserAllowed = userAllowed();
+            if (!currentUserAllowed)
+            {
+                return null;
+            }
+
+            var actors = getActors();
+            return actors.FirstOrDefault();
+        }
+    }
+}
--- samples/Program.cs
+++ samples/Program.cs
@@ -1,24 +1,16 @@
 ﻿namespace Botwin.Samples
 {
-    using System;
-    using System.IO;
-    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore;
     using Microsoft.AspNetCore.Hosting;
 
     class Program
     {
         static void Main(string[] args)
         {
-            using (var host = new WebHostBuilder()
-                    .UseContentRoot(Directory.GetCurrentDirectory())
-                    .UseKestrel()
-                    .UseStartup<Startup>()
-                    .UseUrls("http://*:5000")
-                    .Build())
-            {
-                Console.WriteLine("Starting application...");
-                host.Run();
-            }
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .Build()
+                .Run();
         }
     }
 }
