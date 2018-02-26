6c02a7b1bbf8cf5fac71a7211c520ff9b48a2333
Jonathan Channon <jonathan.channon@gmail.com>
2018-02-26 22:44:42 +0000

    Add check for IValidator in referenced libraries (#85)

---
+++ samples/ValidatorOnlyProject/CastMemberValidator.cs
@@ -0,0 +1,12 @@
+﻿namespace ValidatorOnlyProject
+{
+    using FluentValidation;
+
+    public class CastMemberValidator : AbstractValidator<CastMember>
+    {
+        public CastMemberValidator()
+        {
+            this.RuleFor(x => x.Name).NotEmpty();
+        }
+    }
+}
