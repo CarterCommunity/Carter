eccee8ecf928e766c8e930600b5052b64ecf5686
Joe Stead <joestead1@gmail.com>
2018-05-15 13:16:56 +0100

    To string date for output format

--- test/Carter.Tests/TestModule.cs
+++ test/Carter.Tests/TestModule.cs
@@ -69,7 +69,7 @@ namespace Carter.Tests
             this.Get("/parameterized/{name:alpha}", ctx => ctx.Response.WriteAsync("echo " + ctx.GetRouteData().Values["name"]));
             this.Get("/parameterized/{id:int}", ctx => ctx.Response.WriteAsync("echo " + ctx.GetRouteData().As<int>("id")));
             this.Get("/parameterized/{id:guid}", ctx => ctx.Response.WriteAsync("echo " + ctx.GetRouteData().As<Guid>("id")));
-            this.Get("/parameterized/{id:datetime}", ctx => ctx.Response.WriteAsync("echo " + ctx.GetRouteData().As<DateTime>("id")));
+            this.Get("/parameterized/{id:datetime}", ctx => ctx.Response.WriteAsync("echo " + ctx.GetRouteData().As<DateTime>("id").ToString("dd/MM/yyyy hh:mm:ss")));
 
             this.Post("/", async ctx => { await ctx.Response.WriteAsync("Hello"); });
             this.Put("/", async ctx => { await ctx.Response.WriteAsync("Hello"); });
