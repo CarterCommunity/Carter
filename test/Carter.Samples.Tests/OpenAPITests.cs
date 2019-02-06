cc5c4387172fa6398fada4c300431b785ecb3d8c
Jonathan Channon <jonathan.channon@gmail.com>
2019-02-06 20:48:00 +0000

    openapi json sample unit tested (#160)

---
+++ test/Carter.Samples.Tests/OpenAPITests.cs
@@ -0,0 +1,43 @@
+namespace Carter.Samples.Tests
+{
+    using System.Net.Http;
+    using System.Threading.Tasks;
+    using CarterSample;
+    using Microsoft.AspNetCore;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.AspNetCore.Hosting.Server.Features;
+    using Microsoft.AspNetCore.Http.Features;
+    using Microsoft.AspNetCore.TestHost;
+    using Shouldly;
+    using Shouldly.Configuration;
+    using Xunit;
+
+    public class OpenAPITests
+    {
+        private HttpClient client;
+
+        public OpenAPITests()
+        {
+            var featureCollection = new FeatureCollection();
+            featureCollection.Set<IServerAddressesFeature>(new ServerAddressesFeature());
+
+            var server = new TestServer(WebHost.CreateDefaultBuilder()
+                    .UseStartup<Startup>(), featureCollection
+            );
+
+            this.client = server.CreateClient();
+        }
+
+        [Fact]
+        public async Task Should_return_Carter_approved_OpenAPI_json()
+        {
+            ShouldlyConfiguration.DiffTools.RegisterDiffTool(new DiffTool("diffmerge", new DiffToolConfig {LinuxPath = "/usr/local/bin", MacPath = "/usr/local/bin/diffmerge", WindowsPath = "/usr/local/bin"}, (received, approved, exists) => { return approved; }));
+
+            var res = await this.client.GetAsync("/openapi");
+
+            var content = await res.Content.ReadAsStringAsync();
+
+            content.ShouldMatchApproved();
+        }
+    }
+}
