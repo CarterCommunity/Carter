1add7380f6273fc51f5864b20ab754f45f58ec0b
Jonathan Channon <jonathan.channon@gmail.com>
2019-03-07 21:41:35 +0000

    Don't produce request body on GET (#188)

--- samples/CarterSample/Features/Actors/OpenApi/SampleMetaData.cs
+++ samples/CarterSample/Features/Actors/OpenApi/SampleMetaData.cs
@@ -1,9 +1,12 @@
 namespace CarterSample.Features.Actors
 {
+    using System;
     using Carter.OpenApi;
 
     public class SampleMetaData : RouteMetaData
     {
+        public override Type Request { get; } = typeof(Foo);
+
         public override RouteMetaDataResponse[] Responses { get; } =
         {
             new RouteMetaDataResponse { Code = 200, Response = typeof(PagedItems<Foo>) }
