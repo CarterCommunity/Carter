2f612101422676b5b601d401a41b135b039ad946
William H. Bell <williamhbell@users.noreply.github.com>
2019-11-04 23:29:53 +0000

    Correcting the file permissions

---
+++ src/OpenApi/ApiSchemaAttributes.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Carter.OpenApi
+{
+    public sealed class ApiSchemaAttributes : Attribute
+    {
+        public string Format { get; set; }
+    }
+}
