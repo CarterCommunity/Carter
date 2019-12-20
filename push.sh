fdf22fd4385bf007ad67fdef019d3ab9790abd4c
Jonathan Channon <jonathan.channon@gmail.com>
2019-12-20 16:27:24 +0000

    push correct place

--- push.sh
+++ push.sh
@@ -3,5 +3,5 @@ set -euo pipefail
 
 for package in $(find -name "*.nupkg" | grep "test" -v); do
   echo "${0##*/}": Pushing $package...
-  dotnet nuget push $package --source https://www.myget.org/F/adamralph-ci/api/v2/package --api-key $1
+  dotnet nuget push $package --source https://f.feedz.io/carter/carter/nuget/index.json --api-key $1
 done
