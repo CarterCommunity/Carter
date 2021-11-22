db76aea19e91c17d5361e0ce5ca45d87f2fa1851
Jonathan Channon <jonathan.channon@gmail.com>
2021-11-22 10:50:17 +0000

    Update template project (#287)

--- template/content/HomeModule.cs
+++ template/content/HomeModule.cs
@@ -1,11 +1,4 @@
-﻿namespace CarterTemplate
+﻿public class HomeModule : ICarterModule
 {
-    using Carter;
-    using Microsoft.AspNetCore.Builder;
-    using Microsoft.AspNetCore.Routing;
-
-    public class HomeModule : ICarterModule
-    {
-        public void AddRoutes(IEndpointRouteBuilder app) => app.MapGet("/", () => "Hello from Carter!");
-    }
+    public void AddRoutes(IEndpointRouteBuilder app) => app.MapGet("/", () => "Hello from Carter!");
 }
