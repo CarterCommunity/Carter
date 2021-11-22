5a63008ba98cae9e265ebdf6e8192adab60acb7f
Jonathan Channon <jonathan.channon@gmail.com>
2021-11-22 10:35:58 +0000

    Update template project

---
+++ template/content/GlobalUsings.cs
@@ -0,0 +1,3 @@
+global using Carter;
+global using Microsoft.AspNetCore.Builder;
+global using Microsoft.AspNetCore.Routing;
--- template/content/Program.cs
+++ template/content/Program.cs
@@ -1,6 +1,3 @@
-using Carter;
-using Microsoft.AspNetCore.Builder;
-
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddCarter();
