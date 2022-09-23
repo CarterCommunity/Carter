1a76aae439c2e223c20aad4e5e1a0068f092ab31
Mickaël Derriey <mderriey@gmail.com>
2022-09-23 16:11:39 +0200

    First attempt with plenty of different workflows

--- push.sh
+++ push.sh
@@ -1,7 +1,21 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-for package in $(find -name "*.nupkg" | grep "test" -v); do
-  echo "${0##*/}": Pushing $package...
-  dotnet nuget push $package --source https://f.feedz.io/carter/carter/nuget/index.json --api-key $1
+TARGET_PACKAGE=$1
+if [ "$TARGET_PACKAGE" = "carter" ]; then
+  TARGET_PACKAGE_PATH="./src/Carter/**/*.nupkg"
+elif [ "$TARGET_PACKAGE" = "newtonsoft" ]; then
+  TARGET_PACKAGE_PATH="./src/Carter.ResponseNegotiators.Newtonsoft/**/*.nupkg"
+fi
+
+NUGET_TARGET_SERVICE=$2
+if [ "$NUGET_TARGET_SERVICE" = "feedz" ]; then
+  NUGET_TARGET_URL="https://f.feedz.io/carter/carter/nuget/index.json"
+elif [ "$NUGET_TARGET_SERVICE" = "nuget" ]; then
+  NUGET_TARGET_URL="https://somenugeturl/index.json"
+fi
+
+for package in $(find -wholename "$TARGET_PACKAGE_PATH" | grep "test" -v); do
+  echo "${0##*/}": Pushing $package to $NUGET_TARGET_SERVICE \($NUGET_TARGET_URL\)...
+  # dotnet nuget push $package --source https://f.feedz.io/carter/carter/nuget/index.json --api-key $1
 done
