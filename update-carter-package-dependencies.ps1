6244cb1b508860a71801ef8dd3bb146996ed7f81
Mickaël Derriey <mderriey@gmail.com>
2022-11-04 11:13:22 +0100

    Add workflow to update Carter package references in project files

---
+++ update-carter-package-dependencies.ps1
@@ -0,0 +1,95 @@
+[CmdletBinding(DefaultParameterSetName = 'Default')]
+param(
+    [Parameter(ParameterSetName = 'Default')]
+    [switch]
+    $IncludePrerelease,
+
+    [Parameter(ParameterSetName = 'Override')]
+    [string]
+    $OverrideVersion
+)
+
+function Get-LatestCarterPackageVersion {
+    [CmdletBinding()]
+    param(
+        [Parameter()]
+        [switch]
+        $IncludePrerelease
+    )
+
+    #
+    # NuGet Server API docs
+    # https://learn.microsoft.com/en-us/nuget/api/overview
+    #
+
+    $nugetApiServiceIndexUrl = 'https://api.nuget.org/v3/index.json'
+    $serviceIndexResponse = Invoke-RestMethod -Method Get -Uri $nugetApiServiceIndexUrl
+
+    $nugetApiSearchQueryServiceUrl = $serviceIndexResponse.resources |
+        Where-Object '@type' -eq 'SearchQueryService' |
+        Select-Object -First 1 |
+        Select-Object -ExpandProperty '@id'
+
+    #
+    # https://learn.microsoft.com/en-us/nuget/api/search-query-service-resource#search-for-packages
+    #
+    $queryParameters = @{
+        q           = 'Carter'
+        prerelease  = if ($IncludePrerelease) { 'true' } else { 'false' }
+        semVerLevel = '2.0.0'
+    }
+
+    $searchResponse = Invoke-RestMethod `
+        -Method Get `
+        -Uri $nugetApiSearchQueryServiceUrl `
+        -Body $queryParameters
+
+    $latestPackage = $searchResponse.data |
+        Where-Object 'id' -eq 'Carter' |
+        Sort-Object -Property 'version' -Descending |
+        Select-Object -First 1
+
+    Write-Output -InputObject $latestPackage.version
+}
+
+function Update-CarterPackageReferences {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory = $true)]
+        [string]
+        $RootPath,
+
+        [Parameter(Mandatory = $true)]
+        [string]
+        $PackageVersion
+    )
+
+    $projectFiles = Get-ChildItem `
+        -Path $RootPath `
+        -Recurse `
+        -File `
+        -Filter '*.csproj'
+
+    $regexPattern = '(?<beforeVersion><PackageReference\s+Include="Carter"\s+Version=")[\s\S]+?(?<afterVersion>"\s*/>)'
+
+    foreach ($projectFile in $projectFiles) {
+        $contents = Get-Content -Path $projectFile.FullName -Raw -Encoding utf8
+
+        if ([System.Text.RegularExpressions.Regex]::IsMatch($contents, $regexPattern)) {
+            $newContents = [System.Text.RegularExpressions.Regex]::Replace(
+                $contents,
+                $regexPattern,
+                ('${{beforeVersion}}{0}${{afterVersion}}' -f $PackageVersion))
+
+            Set-Content -Path $projectFile.FullName -Value $newContents -Encoding utf8 -NoNewline
+        }
+    }
+}
+
+if ($PSCmdlet.ParameterSetName -eq 'Default') {
+    $targetCarterPackageVersion = Get-LatestCarterPackageVersion -IncludePrerelease:$IncludePrerelease
+} else {
+    $targetCarterPackageVersion = $OverrideVersion
+}
+
+Update-CarterPackageReferences -RootPath $PSScriptRoot -PackageVersion $targetCarterPackageVersion
