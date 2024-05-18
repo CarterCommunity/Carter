28632c94472f2b36f4c09bbaf0ae2c3105ebcc55
Jonathan Channon <jonathan.channon@gmail.com>
2024-05-18 18:02:29 +0100

    Add ValidateAsync method to use FV async validation

--- samples/ValidatorOnlyProject/CastMemberValidator.cs
+++ samples/ValidatorOnlyProject/CastMemberValidator.cs
@@ -1,12 +1,17 @@
 ﻿namespace ValidatorOnlyProject
 {
+    using System.Threading.Tasks;
     using FluentValidation;
 
     public class CastMemberValidator : AbstractValidator<CastMember>
     {
         public CastMemberValidator()
         {
-            this.RuleFor(x => x.Name).NotEmpty();
+            this.RuleFor(x => x.Name).NotEmpty().MustAsync(async (name, cancellation) =>
+            {
+                await Task.Delay(50);
+                return true;
+            });
         }
     }
 }
