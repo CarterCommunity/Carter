82cf0bd911351eabe905ca0758675a840b3877b1
Jonathan Channon <jonathan.channon@gmail.com>
2025-10-09 14:57:32 +0100

    Add sample, remove setting factory use attribute factory by default, make attribute public

--- samples/CarterSample/Features/Actors/ActorValidator.cs
+++ samples/CarterSample/Features/Actors/ActorValidator.cs
@@ -2,6 +2,7 @@ namespace CarterSample.Features.Actors;
 
 using FluentValidation;
 
+[ValidatorLifetimeAttribute(ServiceLifetime.Scoped)]
 public class ActorValidator : AbstractValidator<Actor>
 {
     public ActorValidator()
